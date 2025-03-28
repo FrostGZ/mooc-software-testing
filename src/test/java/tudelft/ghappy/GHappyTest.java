c5317678cc04b0cf05d2eba702400ee716558a68
FrostGZ <velociraptoruhc@gmail.com>
2025-03-28 15:09:50 -0500

    test(ghappy): corrige y agrega pruebas para verificar "g" felices
    Se corrigió la implementación del método "gHappy" para manejar correctamente los casos donde las "g" no están juntas y se añadieron pruebas automatizadas que verifican distintos escenarios.

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,21 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 public class GHappyTest {
+    @Test
+    public void testGHappy() {
+        GHappy ghappy = new GHappy();
+        Assertions.assertTrue(ghappy.gHappy("xxggxx"));
+        Assertions.assertTrue(ghappy.gHappy("gg"));
+        Assertions.assertTrue(ghappy.gHappy("ggg"));
+        Assertions.assertTrue(ghappy.gHappy(""));
+        Assertions.assertTrue(ghappy.gHappy(null));
+
+        Assertions.assertFalse(ghappy.gHappy("xxgxx"));
+        Assertions.assertFalse(ghappy.gHappy("xxggyygxx"));
+        Assertions.assertFalse(ghappy.gHappy("g"));
+        Assertions.assertFalse(ghappy.gHappy("gxxg"));
+
+    }
 }
