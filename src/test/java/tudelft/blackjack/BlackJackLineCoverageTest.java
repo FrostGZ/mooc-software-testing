4f4d47808c82cb2d521af9978ecc13746159f1d9
FrostGZ <velociraptoruhc@gmail.com>
2025-04-18 17:02:27 -0500

    test(blackjack): agregar casos adicionales para lograr cobertura del 100%.

--- src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
+++ src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
@@ -16,4 +16,22 @@ public class BlackJackLineCoverageTest {
         int result = new BlackJack().play(10, 9);
         Assertions.assertEquals(10, result);
     }
+
+    @Test
+    public void rightPlayerWins() {
+        int result = new BlackJack().play(8, 20);
+        Assertions.assertEquals(20, result);
+    }
+
+    @Test
+    public void bothPlayersSameValid() {
+        int result = new BlackJack().play(18, 18);
+        Assertions.assertEquals(18, result);
+    }
+
+    @Test
+    public void onePlayerBusts() {
+        int result = new BlackJack().play(22, 19);
+        Assertions.assertEquals(19, result);
+    }
 }
