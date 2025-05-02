d77b591173498aa1a36880e70153348c863543e2
FrostGZ <velociraptoruhc@gmail.com>
2025-05-02 17:50:57 -0500

    test(mirror): agregar pruebas unitarias para casos simétricos y no simétricos

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,43 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MirrorTest {
+
+    @Test
+    public void testTextoSimetricoCompleto() {
+        Mirror mirror = new Mirror();
+        assertEquals("racecar", mirror.mirrorEnds("racecar"));
+    }
+
+    @Test
+    public void testInicioIgualFinal() {
+        Mirror mirror = new Mirror();
+        assertEquals("ab", mirror.mirrorEnds("abXYZba"));
+    }
+
+    @Test
+    public void testMitadSimetrica() {
+        Mirror mirror = new Mirror();
+        assertEquals("abccba", mirror.mirrorEnds("abccba"));
+    }
+
+    @Test
+    public void testSinCoincidencia() {
+        Mirror mirror = new Mirror();
+        assertEquals("", mirror.mirrorEnds("abcdef"));
+    }
+
+    @Test
+    public void testUnCaracter() {
+        Mirror mirror = new Mirror();
+        assertEquals("a", mirror.mirrorEnds("a"));
+    }
+
+    @Test
+    public void testCadenaVacia() {
+        Mirror mirror = new Mirror();
+        assertEquals("", mirror.mirrorEnds(""));
+    }
 }
